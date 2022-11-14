551a9a9f599f47df20ec437752c8c22ac8c68ef9
cleal97 <cleal1897@hotmail.com>
2022-11-14 00:01:26 -0300

    creando proyecto hug
    primera version... falta mejorar mucho

---
+++ script.js
@@ -0,0 +1,13 @@
+function cambiaMensaje(){
+    document.getElementById("loading-message").innerText = "Enviado!"
+}
+
+
+setTimeout(cambiaMensaje,2000)
+
+
+setTimeout(function() {
+    alert("Recibiste MIL besos y MIL abrazos!")
+    alert("Te amo mucho!")
+    alert("Muchisimo! ;)")
+}, 4400)
